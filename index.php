02ea0da18728b8febfda97221454f4e1a2545466
AntonBaglaev <abaglaev@yandex.ru>
2023-07-30 21:46:41 +0300

    index.php fix убрад дубль меню

--- index.php
+++ index.php
@@ -4,25 +4,5 @@ $APPLICATION->SetTitle("");
 ?>
 
 
-<?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"horizontal_multilevel", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "section",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "2",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "main",
-		"USE_EXT" => "N",
-		"COMPONENT_TEMPLATE" => "horizontal_multilevel"
-	),
-	false
-);?>
-
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
