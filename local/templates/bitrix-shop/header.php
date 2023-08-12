18df3fe20a81fdb551becfee1e0f4b34aa207b7c
AntonBaglaev <abaglaev@yandex.ru>
2023-08-12 22:57:29 +0300

    fix 5 15.04

--- local/templates/bitrix-shop/header.php
+++ local/templates/bitrix-shop/header.php
@@ -11,6 +11,7 @@ $CurUri = $APPLICATION->GetCurUri();
      // CSS;
      Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/bootstrap.min.css');
      Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/fonts/Roboto/stylesheet.min.css');
+     Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/fonts/fontawesome/font-awesome.min.css'); //TODO удалить когда будут svg спрайты
      Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/js/fancy/jquery.fancybox.min.css');
     // JS
     CJSCore::Init(array("jquery3"));
@@ -43,23 +44,80 @@ $CurUri = $APPLICATION->GetCurUri();
 <?$APPLICATION->ShowPanel();?>
 
 
-<?$APPLICATION->IncludeComponent(
+<div class="top_header">
+    <div class="container">
+        <div class="top_header_content">
+            <button type="" class="top_header_content_menu">
+                <i class="fa fa-bars"></i>
+                <span>
+                    <img src="<?=SITE_TEMPLATE_PATH?>/img/logo_header_new.svg" alt="Bxstore">
+                </span>
+            </button>
+            <?// Меню - http://dev.1c-bitrix.ru/user_help/settings/settings/components_2/navigation/menu.php
+            $APPLICATION->IncludeComponent(
 	"bitrix:menu", 
-	"top_panel_menu", 
+	"horizontal_multilevel", 
 	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "section",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "2",
+		"ROOT_MENU_TYPE" => "top_catalog",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_USE_GROUPS" => "Y",
 		"MENU_CACHE_GET_VARS" => array(
 		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "main",
+		"MAX_LEVEL" => "2",
+		"CHILD_MENU_TYPE" => "top_catalog",
 		"USE_EXT" => "Y",
-		"COMPONENT_TEMPLATE" => "top_panel_menu"
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N",
+		"COMPONENT_TEMPLATE" => "horizontal_multilevel"
 	),
 	false
-);?>
+); ?>
+            <?// Поиск по заголовкам - http://dev.1c-bitrix.ru/user_help/settings/search/components_2/search_title.php
+            $APPLICATION->IncludeComponent(
+            	"bitrix:search.title",
+            	".default",               // [mobile, .default, catalog, visual]
+            	array(
+            		// region Основные параметры
+            		"NUM_CATEGORIES"      =>  "1",                           // Количество категорий поиска
+            		"TOP_COUNT"           =>  "5",                           // Количество результатов в каждой категории
+            		"ORDER"               =>  "date",                        // Сортировка результатов : array ( 'date' => 'по дате', 'rank' => 'по релевантности', )
+            		"USE_LANGUAGE_GUESS"  =>  "Y",                           // Включить автоопределение раскладки клавиатуры
+            		"CHECK_DATES"         =>  "N",                           // Искать только в активных по дате документах
+            		"SHOW_OTHERS"         =>  "N",                           // Показывать категорию 'прочее'
+            		// endregion
+            		// region Шаблоны ссылок
+            		"PAGE"                =>  "#SITE_DIR#search/index.php",  // Страница выдачи результатов поиска (доступен макрос #SITE_DIR#)
+            		// endregion
+            		// region Категория #1
+            		"CATEGORY_0_TITLE"    =>  "",                            // Название категории
+            		"CATEGORY_0"          =>  array('all'),                  // Ограничение области поиска : array ( 'no' => 'Не ограничивать', 'main' => '[main] Статические файлы', 'forum' => '[forum] Форумы для поиска', 'iblock_catalog' => '[iblock_catalog] Каталоги', 'iblock_news' => '[iblock_news] Новости', 'iblock_offers' => '[iblock_offers] Торговые предложения', 'iblock_services' => '[iblock_services] Сервисы', 'iblock_references' => '[iblock_references] Справочники', 'blog' => '[blog] Блоги', 'microblog' => '[microblog] Микроблоги', 'socialnetwork' => '[socialnetwork] Социальная сеть (группы)', 'socialnetwork_user' => '[socialnetwork_user] Социальная сеть (пользователь)', )
+            		// endregion
+            	)
+            );?>
+            <div class="top_header_content_phone">
+                <a href="tel:89000000000" rel="nofollow">+7 900 000 00 00</a>
+                <button type="button" class="top_header_content_phone_button"></button>
+            </div>
+            <div class="top_header_content_user">
+                <a href="" class="top_header_content_user_auth">
+
+                </a>
+                <a href="" class="top_header_content_user_wishlist">
+
+                </a>
+                <?// Малая корзина
+                $APPLICATION->IncludeComponent(
+                	"bitrix:sale.basket.basket.small",
+                	".default",
+                	array(
+
+                	)
+                );?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
