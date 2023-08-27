2a7db4eb2b20bf2b439406abf65028b47ac67693
AntonBaglaev <abaglaev@yandex.ru>
2023-08-27 17:00:53 +0300

    добавлен шаблон регистрации

--- personal/auth/registration.php
+++ personal/auth/registration.php
@@ -5,17 +5,15 @@ $APPLICATION->SetPageProperty("title", "Регистрация");
 $APPLICATION->SetPageProperty("keywords", "Регистрация");
 $APPLICATION->SetPageProperty("description", "Регистрация");
 $APPLICATION->SetTitle("Регистрация");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.register",
-	"",
-	Array(
-		"AUTH" => "Y",
-		"REQUIRED_FIELDS" => array(),
-		"SET_TITLE" => "Y",
-		"SHOW_FIELDS" => array(),
-		"SUCCESS_PAGE" => "/personal/",
-		"USER_PROPERTY" => array(),
-		"USER_PROPERTY_NAME" => "",
-		"USE_BACKURL" => "Y"
-	)
+?><?$APPLICATION->IncludeComponent("bitrix:main.register", "flat", Array(
+	"AUTH" => "Y",	// Автоматически авторизовать пользователей
+		"REQUIRED_FIELDS" => "",	// Поля, обязательные для заполнения
+		"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+		"SHOW_FIELDS" => "",	// Поля, которые показывать в форме
+		"SUCCESS_PAGE" => "/personal/",	// Страница окончания регистрации
+		"USER_PROPERTY" => "",	// Показывать доп. свойства
+		"USER_PROPERTY_NAME" => "",	// Название блока пользовательских свойств
+		"USE_BACKURL" => "Y",	// Отправлять пользователя по обратной ссылке, если она есть
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
